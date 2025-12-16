5a7af58c7bd1311a6900632945b8e7f5b07b5b09
Vichu95 <k.vishnudev@gmail.com>
2025-12-16 13:12:59 +0100

    Removed warning

--- PulseMobTracker/app/src/main/java/com/focux/pulse/ui/theme/Theme.kt
+++ PulseMobTracker/app/src/main/java/com/focux/pulse/ui/theme/Theme.kt
@@ -42,6 +42,7 @@ fun PulseTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
+            @Suppress("DEPRECATION")
             window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
         }
