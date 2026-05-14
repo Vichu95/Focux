8a739dc7205e21f2210094cce5eba99e8265b419
Vichu95 <k.vishnudev@gmail.com>
2026-05-14 19:08:00 +0200

    version 5 2.0 trying to release

--- PulseMobTracker/app/build.gradle.kts
+++ PulseMobTracker/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.focux.pulse"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 5
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
